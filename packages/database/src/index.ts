e1eb017c2096f8d436d8695971ac0b4c5b1fae62
chmodmasx <daninicob@gmail.com>
2026-09-15 11:51:07 -0300

    feat(database): export ledger repository

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -5,3 +5,4 @@ export * from "./scheduler-repository.ts";
 export * from "./simulation-step-repository.ts";
 export * from "./cognition-repository.ts";
 export * from "./routine-repository.ts";
+export * from "./ledger-repository.ts";
